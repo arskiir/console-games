cc1f92229a871088b43c0ffdfce25010054d771a
Sukrit Banthao <arm.xdsk@gmail.com>
2023-01-19 10:29:18 +0700

    remove dead code

--- src/util.rs
+++ src/util.rs
@@ -5,19 +5,5 @@ pub fn get_char_input() -> char {
     while input == "" {
         stdin().read_line(&mut input).expect("Failed to read input");
     }
-    // println!();
     input.chars().next().unwrap()
-}
-
-pub fn get_unique_vec<T>(iter: impl Iterator<Item = T>) -> Vec<T>
-where
-    T: std::cmp::PartialEq,
-{
-    let mut unique_vec: Vec<T> = Vec::new();
-    for item in iter {
-        if !unique_vec.contains(&item) {
-            unique_vec.push(item);
-        }
-    }
-    unique_vec
 }
