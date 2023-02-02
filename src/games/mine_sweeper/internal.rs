d18ab46675bcf6780b2066fae8a514d9731524e0
Arm Sukrit <arm.xdsk@gmail.com>
2023-02-02 11:10:53 +0700

    starts with alphabet coords first for minesweeper, ux++

--- src/games/mine_sweeper/internal.rs
+++ src/games/mine_sweeper/internal.rs
@@ -7,8 +7,8 @@ use crate::util::probability;
 use self::cell::Cell;
 
 const COORD_SYMBOLS: [char; 35] = [
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 ];
 const MINE: &str = "⊛";
 const FLAGGED: &str = "⚑";
